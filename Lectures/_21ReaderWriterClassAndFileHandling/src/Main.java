b19e54c44be6d38994682a49c2cd3f446a414fa0
Prasad Bhakare <prasadbhakare.5@gmail.com>
2025-04-21 01:24:12 +0530

    Actual File Handling

--- Lectures/_21ReaderWriterClassAndFileHandling/src/Main.java
+++ Lectures/_21ReaderWriterClassAndFileHandling/src/Main.java
@@ -1,5 +1,6 @@
 
 import java.io.*;
+import java.nio.Buffer;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -15,11 +16,35 @@ public class Main {
 //        } catch (IOException e){
 //            System.out.println(e.getMessage());
 //        }
-        try(BufferedReader r = new BufferedReader(new StringReader("this is a string"))){
-            System.out.println(r.readLine());
+//        try(BufferedReader r = new BufferedReader(new StringReader("this is a string"))){
+//            System.out.println(r.readLine());
+//        } catch (IOException e){
+//            System.out.println(e.getMessage());
+//        }
+
+        //Actual file handling
+        try{
+            File fo = new File("new-file.txt");
+            fo.createNewFile();
+            //the fo.delete() returns boolean value
+//            if(fo.delete()){
+//                System.out.println("Deleted the file "+fo.getName());
+//            }
+        } catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+
+        try(FileWriter fw = new FileWriter("new-file.txt")){
+            fw.write("This is written by the file writer.");
+            fw.append(" This should be appended");
         } catch (IOException e){
             System.out.println(e.getMessage());
         }
 
+        try(BufferedReader fr = new BufferedReader(new FileReader("new-file.txt"))){
+            System.out.println(fr.readLine());
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
