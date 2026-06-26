1653359c9e8333ecde32f9ef0b0d82c23b847bc3
Prasad <prasadbhakare.5@gmail.com>
2026-06-26 17:56:15 +0530

    Insertion Sort

--- Lectures/_5SortingAlgorithms/src/_3InsertionSort.java
+++ Lectures/_5SortingAlgorithms/src/_3InsertionSort.java
@@ -7,18 +7,18 @@ public class _3InsertionSort {
         System.out.println(Arrays.toString(arr));
     }
     static void insertion(int[] arr){
-        for (int i = 0; i < arr.length - 1; i++) {
-            for (int j = i + 1; j > 0; j--) {
-                if(arr[j] < arr[j - 1]){
-                    //swap
-                    int temp = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = temp;
-                }
-                else {
-                    break;
-                }
+        int n = arr.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while(j >= 0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
             }
+
+            arr[j+1] = key;
         }
     }
 }
